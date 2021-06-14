53b82675cb9291e11ddc9ab80056e72eb6f00205
Guilhem Lavaux <guilhem.lavaux@iap.fr>
2021-06-14 19:18:16 +0200

    Add configure file loader

---
+++ src/euclid_obssys/config.py
@@ -0,0 +1,18 @@
+
+
+def readConfig(fname: str) -> Dict:
+    """Read a pipeline configuration file.
+
+    That configuration file is written in python
+
+    Args:
+        fname (str): File path of the configuration file
+
+    Returns:
+        dict: A dictionnary with a number of definitions and functions
+    """
+   with open(fname, mode="rt") as f:
+     code='\n'.join(f.read().splitlines())
+   global_dict = {'__builtins__':__builtins__}
+   exec(code, global_dict)
+   return global_dict
