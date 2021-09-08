d6a1ffb81fab6af41ef88be6cd02f309b6ec6590
Guilhem Lavaux <guilhem.lavaux@iap.fr>
2021-09-08 17:37:57 +0200

    Reformat

--- src/euclid_obssys/tool/create_fulloctant_footprint.py
+++ src/euclid_obssys/tool/create_fulloctant_footprint.py
@@ -10,7 +10,8 @@ from ..config import readConfig
 def createFootprint(outdir: str = "Products") -> None:
     import numpy as np
     import matplotlib
-    matplotlib.use('Agg')
+
+    matplotlib.use("Agg")
     import matplotlib.pyplot as plt
     from astropy.io import fits
     import healpy as hp
@@ -20,45 +21,54 @@ def createFootprint(outdir: str = "Products") -> None:
 
     # these are the parameters of the footprint
 
-    footprint_fname = path.join(outdir, "Footprints","FullOctant.fits")
+    footprint_fname = path.join(outdir, "Footprints", "FullOctant.fits")
     footprint_res = 2048
     footprint_tag = None
     footprint_zrange = [0.8, 2.0]
 
     # reddening map for checking where the footprint has been placed
-    #reddening = hp.ud_grade(hp.read_map(outdir+'ExtinctionMaps/HFI_CompMap_ThermalDustModel_2048_R1.20.fits', field=2), footprint_res)
-    reddening = hp.ud_grade(hp.read_map(path.join(outdir,"ExtinctionMaps","COM_CompMap_ThermalDust-commander_2048_R2.00.fits", field=2), footprint_res)
+    # reddening = hp.ud_grade(hp.read_map(outdir+'ExtinctionMaps/HFI_CompMap_ThermalDustModel_2048_R1.20.fits', field=2), footprint_res)
+    reddening = hp.ud_grade(
+        hp.read_map(
+            path.join(
+                outdir,
+                "ExtinctionMaps",
+                "COM_CompMap_ThermalDust-commander_2048_R2.00.fits",
+            ),
+            field=2,
+        ),
+        footprint_res,
+    )
 
     # creating boolean healpy map
-    (theta,phi)=hp.pix2ang(footprint_res,np.arange(hp.nside2npix(footprint_res)))
-    footprint=np.zeros(hp.nside2npix(footprint_res),dtype=bool)
+    (theta, phi) = hp.pix2ang(footprint_res, np.arange(hp.nside2npix(footprint_res)))
+    footprint = np.zeros(hp.nside2npix(footprint_res), dtype=bool)
 
     # square of 10 deg of size on the equator
-    footprint[(theta<np.pi/2.) & (phi<np.pi/2.)]=True
-    sky_fraction = footprint.sum()/footprint.size
+    footprint[(theta < np.pi / 2.0) & (phi < np.pi / 2.0)] = True
+    sky_fraction = footprint.sum() / footprint.size
 
     # plot of the location
     foot2 = reddening.copy()
     foot2[footprint] *= 2
-    #hp.mollview(footprint.astype(int)+reddening,max=100)
-    hp.mollview(foot2,max=1000)
-    plt.savefig(path.join(outdir,"Plots","FullOctant.png")
+    # hp.mollview(footprint.astype(int)+reddening,max=100)
+    hp.mollview(foot2, max=1000)
+    plt.savefig(path.join(outdir, "Plots", "FullOctant.png"))
 
     # writes footprint on fits file
     print("## writing footprint on file {}".format(footprint_fname))
 
-    fg = fits.Column( name='FOOTPRINT_G', array=footprint, format='L')
+    fg = fits.Column(name="FOOTPRINT_G", array=footprint, format="L")
     tb = fits.BinTableHDU.from_columns([fg])
-    tb.header.append('RES')
-    tb.header['RES']=footprint_res
-    tb.header.append('MINZ')
-    tb.header['MINZ']=footprint_zrange[0]
-    tb.header.append('MAXZ')
-    tb.header['MAXZ']=footprint_zrange[1]
-    tb.header.append('TAG')
-    tb.header['TAG']=footprint_tag
-    tb.header.append('SKYFRAC')
-    tb.header['SKYFRAC']=sky_fraction
+    tb.header.append("RES")
+    tb.header["RES"] = footprint_res
+    tb.header.append("MINZ")
+    tb.header["MINZ"] = footprint_zrange[0]
+    tb.header.append("MAXZ")
+    tb.header["MAXZ"] = footprint_zrange[1]
+    tb.header.append("TAG")
+    tb.header["TAG"] = footprint_tag
+    tb.header.append("SKYFRAC")
+    tb.header["SKYFRAC"] = sky_fraction
 
     tb.writeto(footprint_fname, overwrite=True)
-
