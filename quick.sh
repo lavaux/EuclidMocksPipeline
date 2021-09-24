2bcb6c826c24d551d69d01df7ac914d3dea7e071
Guilhem Lavaux <guilhem.lavaux@iap.fr>
2021-09-24 17:29:24 +0200

    Update quick

--- quick.sh
+++ quick.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+set -e
+
 PYTHON=$(which python3)
 BASETOOL="$PYTHON -m euclid_obssys.tool"
+VIEWTOOL="$PYTHON -m euclid_obssys.view" 
 
 FOOTPRINT="createFootprint"
 #FOOTPRINT="createFullOctantFootprint"
@@ -22,35 +25,32 @@ $BASETOOL createSmoothHOD $CONFIG     # measures the HOD from a flagship master
 
 
 $BASETOOL createSDHOD_Catalog $CONFIG              # creates an SDHOD galaxy catalog from a halo catalog
-$BASETOOL dN_dZ $CONFIG                            # measures the n(z) of a galaxy catalog
 
+fi
+$BASETOOL dN_dZ $CONFIG                            # measures the n(z) of a galaxy catalog
 $BASETOOL createRandom $CONFIG                  # creates the random (before selection)
 
-$BASETOOL createSelection $CONFIG  # creates a selection for galaxy
 $BASETOOL createSelection --use-data=False $CONFIG       # creates a selection for random
-fi
-
+$BASETOOL createSelection $CONFIG  # creates a selection for galaxy
 
 $BASETOOL writeCatalogs4LE3 $CONFIG               # writes the catalog in redshift bins
 
+$BASETOOL numbercounts $CONFIG                    # number counts for redshift bins
+
+$VIEWTOOL angular_map Products/GalaxyCatalogs/catalogs_hodcat_8614_100sqdeg_m1_cMdiemer19.fits Products/Selections/catalogs_data_lutmw_catalogs_hodcat_8614_100sqdeg_m1_cMdiemer19.fits 0 1 --Nside 1024 --output angular_default.pdf
+
+$VIEWTOOL visualizeHOD $CONFIG                    
+
+#maps_and_cls.py input                    # measurement of angular clustering  WORK IN PROGRESS
+$VIEWTOOL dn_dz $CONFIG
+
 exit 0
 
-
-createSDHODfromPinocchio.py input        # creates a set of galaxy catalog from pinocchio lightcones
-
-
-
-
-
-numbercounts.py input                    # number counts for redshift bins
-
-maps_and_cls.py input                    # measurement of angular clustering  WORK IN PROGRESS
+#createSDHODfromPinocchio.py input        # creates a set of galaxy catalog from pinocchio lightcones
 
 createPkScripts.py input                 # creates the script and param files for PK
 
 # plotting scritps
-visualizeHOD.py input                    
-plot_dndz.py input
 plot_numbercounts.py input
 plot_pk.py input
 plot_cls.py input                   WORK IN PROGRESS
